a6e9f1f1d763b955ba479648123d8784b900cc9a
HyeongChang Lee <hc9904@hanyang.ac.kr>
2023-11-16 21:22:32 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231116/코딩톡/coding-talk.cpp
+++ 231116/코딩톡/coding-talk.cpp
@@ -7,22 +7,27 @@ int pro[27];
 int main() {
     // 여기에 코드를 작성해주세요.
     int n,p,m,u,preu=-1;
-    char c;
+    char c,prec;
     cin >> n >> m >> p ; // max pro, msg size, check num
     p--;
     for(int i=0;i<m;i++){
         cin >> c >> u; //pro, num
-        if (u!=0) {
-            if (i>=p){ // 뒷사람들
-                pro[c-'A']=1;
-            } 
-        } else {
-            if (i==p){
-                for(int i=0;i<n;i++){
-                    pro[i]=1;
-                }
+        if (i>=p){ // 뒷사람들
+            pro[c-'A']=1;
+        } 
+        // 앞번호
+        if (u==0 && i>=p) { // 다봄
+            for(int i=0;i<n;i++){
+                pro[i]=1;
             }
+            break;
+        } 
+        // 앞에 봤던 사람일때
+        if (preu==u){
+            pro[prec-'A']=1;
         }
+        prec=c;
+        preu=u;
     }
 
     for(int i=0;i<n;i++){
