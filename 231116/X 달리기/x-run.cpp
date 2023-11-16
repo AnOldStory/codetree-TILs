89ed44d701fbbed357582a9954e1333580e4ab58
HyeongChang Lee <hc9904@hanyang.ac.kr>
2023-11-16 21:54:22 +0900

    Title: X 달리기, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231116/X 달리기/x-run.cpp
+++ 231116/X 달리기/x-run.cpp
@@ -2,15 +2,15 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int X,i,dis;
+    int X,speed,dis;
     scanf("%d",&X);
     int ans=0;
-    for(i=1;dis*2<X;i++){
+    for(speed=1;dis*2<X;speed++){
         ans++;
-        dis+=i;
+        dis+=speed;
     }
     ans=ans*2;
-    if(X-dis*2-i>0){
+    if(X-dis*2-speed>0){
         ans++;    
     }
     printf("%d",ans);
