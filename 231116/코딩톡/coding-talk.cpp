fd592929e84d80ad90a7cc8d4067ace32de71838
HyeongChang Lee <hc9904@hanyang.ac.kr>
2023-11-16 21:14:32 +0900

    Title: 코딩톡, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231116/코딩톡/coding-talk.cpp
+++ 231116/코딩톡/coding-talk.cpp
@@ -9,13 +9,19 @@ int main() {
     int n,p,m,u,preu=-1;
     char c;
     cin >> n >> m >> p ; // max pro, msg size, check num
-
+    p--;
     for(int i=0;i<m;i++){
         cin >> c >> u; //pro, num
         if (u!=0) {
-            if (i>=u){
+            if (i>=p){ // 뒷사람들
                 pro[c-'A']=1;
             } 
+        } else {
+            if (i==p){
+                for(int i=0;i<n;i++){
+                    pro[i]=1;
+                }
+            }
         }
     }
 
