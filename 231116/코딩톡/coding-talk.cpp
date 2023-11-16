b80be6b5f03324fd00e11445ca08fda364dd6755
HyeongChang Lee <hc9904@hanyang.ac.kr>
2023-11-16 21:33:04 +0900

    Title: 코딩톡, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231116/코딩톡/coding-talk.cpp
+++ 231116/코딩톡/coding-talk.cpp
@@ -2,36 +2,37 @@
 
 using namespace std;
 
-int pro[27];
+char c[101];
+int u[101];
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,p,m,u,preu=-1;
-    char c,prec;
+    int n,p,m;
     cin >> n >> m >> p ; // max pro, msg size, check num
     p--;
+
     for(int i=0;i<m;i++){
-        cin >> c >> u; //pro, num
-        if (i>=p){ // 뒷사람들
-            pro[c-'A']=1;
-        } 
-        // 앞번호
-        // if (u==0) { // 다봄
-        //     for(int i=0;i<n;i++){
-        //         pro[i]=1;
-        //     }
-        //     break;
-        // } 
-        // 앞에 봤던 사람일때
-        if (preu==u && i==p){
-            pro[prec-'A']=1;
-        }
-        prec=c;
-        preu=u;
+        cin >> c[i] >> u[i];
     }
 
+    if (u[p-1]==0) return 0;
+
     for(int i=0;i<n;i++){
-        if (pro[i]==0) cout << (char) (i+'A')<< ' ';
+        // if (pro[i]==0) cout << (char) (i+'A')<< ' ';
+
+        bool flag = false;
+
+        for(int j=0;j<m;j++){
+            if (u[j]>=u[p] && c[j]-'A' == i) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (flag== false) cout << (char) (i+'A')<< " ";
+
+
     }
+
     return 0;
 }
